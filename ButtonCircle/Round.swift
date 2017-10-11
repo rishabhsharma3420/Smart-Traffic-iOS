62e733ca3cc5b677ba3fdf25b6cddc64e277b00b
RISHABH SHARMA <rishabhsharma3420@gmail.com>
2017-10-11 07:13:21 +0530

    iOS files pushed

---
+++ ButtonCircle/Round.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+@IBDesignable
+class Round: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+}
